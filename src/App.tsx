c1202a8356eaef436f3fa791e7010fc7e1784683
Hamid Tanhaei <hamidt.com@gmail.com>
2025-02-27 14:21:47 +0330

    routes loaded lazy and bundle analyzer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router';
-
 import { MainLayout } from '@/components/layout/MainLayout/MainLayout';
-import { Home } from '@/pages/Home/Home.tsx';
-import { Settings } from '@/pages/Settings/Settings.tsx';
-import { CreditCards } from '@/pages/CreditCards/CreditCards.tsx';
 
-import { browserRoutes } from '@/consts';
+const routes = {
+  home: {
+    path: '/',
+    component: React.lazy(() => import('@/pages/Home/Home').then(m => ({ default: m.Home })))
+  },
+  settings: {
+    path: '/settings',
+    component: React.lazy(() => import('@/pages/Settings/Settings').then(m => ({ default: m.Settings })))
+  },
+  creditCards: {
+    path: '/credit-cards',
+    component: React.lazy(() => import('@/pages/CreditCards/CreditCards').then(m => ({ default: m.CreditCards })))
+  }
+} as const;
 
 function App() {
   return (
     <BrowserRouter>
       <MainLayout>
-        <Routes>
-          <Route path={browserRoutes.home} element={<Home />} />
-          <Route path={browserRoutes.settings} element={<Settings />} />
-          <Route
-            path={browserRoutes['credit-cards']}
-            element={<CreditCards />}
-          />
-        </Routes>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            {Object.values(routes).map(({ path, component: Component }) => (
+              <Route key={path} path={path} element={<Component />} />
+            ))}
+          </Routes>
+        </React.Suspense>
       </MainLayout>
     </BrowserRouter>
   );
